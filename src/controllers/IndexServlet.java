ae443fe8ef43ac4dc48ab7edcc509bf4e10f6d91
yuka <yuka@yukapaso>
2023-07-16 18:20:24 +0900

    Index/NewServletの修正

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -2,11 +2,15 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import utils.DBUtil;
 
 /**
  * Servlet implementation class IndexServlet
@@ -20,15 +24,22 @@ public class IndexServlet extends HttpServlet {
      */
     public IndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+
+        em.close();
+
+        request.setAttribute("messages", messages);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        rd.forward(request, response);
     }
 
 }
